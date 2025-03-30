cf5ba844ad69c15a30b1cc205721121421eba987
Alishba Haroon <156108243+Alishba-Haroon@users.noreply.github.com>
2025-03-30 20:27:24 +0500

    Update app.py

--- LAB TASK 8/app.py
+++ LAB TASK 8/app.py
@@ -1,34 +1,26 @@
-from flask import Flask, render_template, request, jsonify
-import yfinance as yf
-
-
+from flask import Flask, jsonify, render_template, request
+import requests
 app = Flask(__name__)
-
+API_KEY = "S628GKBNREDKJG1C"
+BASE_URL = "https://www.alphavantage.co/query"
 def get_stock_price(symbol):
-    try:
-        stock = yf.Ticker(symbol)
-        history = stock.history(period="1d")
-        if history.empty:
-            return None
-        price = history["Close"].iloc[-1]
-        return round(price, 2)
-    except Exception as e:
-        return None
-    
+    params = {
+        "function": "TIME_SERIES_INTRADAY",
+        "symbol": symbol,
+        "interval": "1min",
+        "apikey": API_KEY
+    }
+    data = requests.get(BASE_URL, params=params).json()
+    return float(data["Time Series (1min)"][next(reversed(data["Time Series (1min)"]))]["4. close"])
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
 @app.route('/get_price', methods=['POST'])
-def get_price():
-    data = request.get_json()
-    if not data or "symbol" not in data:
-        return jsonify({"error": "Stock symbol required"}), 400
-    symbol = data["symbol"].upper()
-    price = get_stock_price(symbol)
-    if price is None:
-        return jsonify({"error": "Invalid stock symbol or no data available"}), 400
-    return jsonify({"symbol": symbol, "price": price})
-
+def price():
+    symbol = request.get_json()["symbol"].upper()
+    try:
+        return jsonify({"symbol": symbol, "price": round(get_stock_price(symbol), 2)})
+    except:
+        return jsonify({"error": "Invalid symbol or no data"}), 400
 if __name__ == '__main__':
     app.run(debug=True)
